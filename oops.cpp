59fbf2a5b62093399572017043d38d275f9704bb
aditya220896 <110400651+aditya220896@users.noreply.github.com>
2023-02-04 18:48:45 +0530

    Update oops.cpp

--- oops.cpp
+++ oops.cpp
@@ -4,14 +4,17 @@ using std:: string;
 class AbstractEmployee {  // whichever class decides to sign this contract need to provide the implementation of method AskForPromotion
     
     virtual void AskForPromotion() = 0; //we will also make this function obligator ie we will force any class that signs this contract will implement this method.
-                                        // we will achieve this by writing 'virtual' keyword ahead.
+                            // we will achieve this by writing 'virtual' keyword ahead.
     };
 
 
 class Employee: AbstractEmployee {
 private:
-    string Name, Company;
+    string  Company;
     int Age;
+    
+protected:
+    string Name;
     
 public:
     void setName (string name) {
@@ -56,10 +59,53 @@ public:
     }
 };
 
+class Developer: public Employee {
+    public :
+        string FavProgrammingLanguage;
+        
+    Developer (string name, string company, int age, string favProgrammingLanguage)
+        : Employee(name, company, age)
+    {   
+        FavProgrammingLanguage = favProgrammingLanguage;
+        
+    }
+    
+    void fixBug () {
+        std::cout<<Name<<" fix bug using "<<FavProgrammingLanguage<<std::endl;
+    }
+};
+
+class Teacher : Employee {
+public: 
+    string Subject;
+    void prepareLesson () {
+        std::cout<<Name<<" is preparing for "<<Subject<<" lesson"<<std::endl;
+    }
+    
+    Teacher (string name, string company, int age, string subject)
+        : Employee(name, company, age)
+        {
+            Subject = subject;
+        }
+};
+
+
+
 int main () {
     Employee employee1 = Employee ("Aditya", "Accenture", 36);
     Employee employee2 = Employee ("Rahul", "Zerodha", 23);
 
     employee1.AskForPromotion();
     employee2.AskForPromotion();
+    
+    Developer obj1 = Developer("Aditya", "Accenture", 26, "C++");
+    obj1.fixBug();
+    obj1.fixBug();
+    obj1.fixBug();
+    obj1.AskForPromotion();
+    
+    Teacher t = Teacher("Jack", "Cool School", 35, "History");
+    t.prepareLesson();
+    
+    
 }
